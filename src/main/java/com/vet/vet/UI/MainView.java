fc8f1ae260a5a7a9e9b6c91400e122d017171829
slmens <selimenes527@outlook.com>
2023-11-27 21:07:42 +0300

    Vaadin setup done

---
+++ src/main/java/com/vet/vet/UI/MainView.java
@@ -0,0 +1,13 @@
+package com.vet.vet.UI;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+
+    }
+
+}
